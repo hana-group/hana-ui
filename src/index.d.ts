9a5f5a50b960405ab75a16fe8c5cda6a2952c14b
hanaarena <hanaarena@gmail.com>
2018-02-08 14:14:16 +0800

    improve: add more declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,9 @@
  * Description:
  */
 export {default as Divider} from './seeds/Divider';
+export {default as Modal} from './seeds/Modal';
+export {default as Sidebar} from './seeds/Sidebar';
+export {default as Table} from './seeds/Table';
+export {default as Title} from './seeds/Title';
+export {Button, ButtonGroup} from './seeds/Button';
+export {Notification, Notifications} from './seeds/Notification';
