61dae0129de980fc05c7ea6c68eb0e8a8febbc1f
DaiTianyu <dtysky@outlook.com>
2018-02-09 13:28:21 +0800

    Add .d.ts files for many components.

---
+++ src/flowers/VideoPlayer/index.d.ts
@@ -0,0 +1,75 @@
+/**
+ * Copyright(c) dtysky<dtysky@outlook.com>
+ * Created: 7 Feb 2018
+ * Description:
+ */
+/// <reference types="react" />
+import {Component} from 'react';
+
+export interface IPropTypes {
+  view?: 'window' | 'full';
+  mode?: 'random' | 'cycle' | 'repeat' | 'normal';
+  preload?: string;
+  autoPlay?: boolean;
+  play?: boolean;
+  currentTime?: number;
+  volume?: number;
+  list?: {
+    title: string,
+    desc?: string,
+    sources: {
+      src: string,
+      type?: string
+    }[],
+    image?: string,
+    text?: string,
+    poster?: string
+  }[];
+  currentItem?: number;
+  defaultText?: string;
+  defaultImage?: string;
+  defaultImage?: string;
+  className?: string;
+  style?: any;
+  children?: JSX.Element;
+}
+export interface IStateTypes {
+  view: 'window', 'full';
+  mode: 'random', 'cycle', 'repeat', 'normal';
+  play: boolean;
+  currentTime: number;
+  currentPercent: number;
+  volume: number;
+  currentItem: number;
+  bufferedPercent: number;
+  openList: boolean;
+}
+export default class VideoPlayer extends Component<IPropTypes, IStateTypes> {
+    static defaultProps: IPropTypes;
+    public state: IStateTypes;
+    private parseProps;
+    private parseSources;
+    private genType;
+    private smartFormatTime;
+    private play;
+    private pause;
+    private handleProgress;
+    private handleTimeUpdate;
+    private handlePlayEnd;
+    private handlePressKey;
+    private handleSwitchPlay;
+    private handleSwitchVolume;
+    private handleChangeVolume;
+    private handleChangeCurrent;
+    private handleSwitchMode;
+    private handleSwitchView;
+    private handleViewChange;
+    private handleSwitchList;
+    private handleChangeItem;
+    public render: () => JSX.Element;
+    private renderVideo;
+    private renderControllers;
+    private renderProgress;
+    private renderList;
+    private renderListItem;
+}
