b8132132d74cca02ba8ecdcc9ec64f273a03157f
DaiTianyu <dtysky@outlook.com>
2018-02-10 16:26:25 +0800

    Fix bugs for d.ts files.

--- src/seeds/Notification/index.d.ts
+++ src/seeds/Notification/index.d.ts
@@ -1,7 +1,32 @@
-import * as Notification from './Notification';
-import * as Notifications from './Notifications';
+/// <reference types="react" />
+import {Component} from 'react';
 
-export {
-  Notification,
-  Notifications
-};
+export interface Props {
+  style?: any;
+  type?: string[];
+  content: string[] | JSX.Element[];
+  duration?: number;
+  showClose?: boolean;
+  className?: string;
+  handleClose?: () => void;
+  onRequestClose?: () => void;
+}
+
+export interface States {
+}
+
+export class Notification extends Component<Props, States> {
+  static defaultProps: Props;
+}
+
+export interface INotificationsProps {
+  notification?: JSX.Element[];
+  onRequestClose?: () => void;
+}
+
+export interface INotificationsStates {
+}
+
+export class Notifications extends Component<INotificationsProps, INotificationsStates> {
+  static defaultProps: Props;
+}
