2c9b6de68d4950533fb7563807ebb7f63e85f454
missrhyme <lovelyrhyme@gmail.com>
2019-01-07 15:14:07 +0800

    fix home page source

--- demo/src/config.js
+++ demo/src/config.js
@@ -22,18 +22,18 @@
 // };
 export default {
   logo: require('../static/images/logo.png'),
-  icon: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/hana-icon.jpg',
+  icon: '/demo/static/images/hana-icon.jpg',
   overview: {
-    intro: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/clover.png',
-    origin: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/violets.png',
-    style: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/cornflower.png',
-    useful: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/sakura.png',
-    rich: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/sunflower.png',
-    understand: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/edelweiss.png'
+    intro: '/demo/static/images/clover.png',
+    origin: '/demo/static/images/violets.png',
+    style: '/demo/static/images/cornflower.png',
+    useful: '/demo/static/images/sakura.png',
+    rich: '/demo/static/images/sunflower.png',
+    understand: '/demo/static/images/edelweiss.png'
   },
   authors: {
-    dtysky: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/dtysky.png',
-    rhyme: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/rhyme.jpg',
-    lanz: 'http://oekm6wrcq.bkt.clouddn.com/hana-ui/lanz.jpg'
+    dtysky: '/demo/static/images/dtysky.png',
+    rhyme: '/demo/static/images/rhyme.jpg',
+    lanz: '/demo/static/images/lanz.jpg'
   }
 };
